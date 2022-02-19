e9ae879728f5cc01e80201fda9a4c39bb4563869
Илья Емец <67930111+Dzirt089@users.noreply.github.com>
2022-02-19 23:38:24 +0300

    Тестовый консольный проект

---
+++ Tests/CV19Cons/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CV19Cons
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
