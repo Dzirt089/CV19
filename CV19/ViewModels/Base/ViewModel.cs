70f83509917cb7b2c2e6f744a0b5a54d5e844597
Илья Емец <67930111+Dzirt089@users.noreply.github.com>
2022-02-20 11:49:58 +0300

    Добавляем интерфейс IDisposable к классу ViewModel для примера реализации

--- CV19/ViewModels/Base/ViewModel.cs
+++ CV19/ViewModels/Base/ViewModel.cs
@@ -6,8 +6,9 @@ using System.Text;
 
 namespace CV19.ViewModels.Base
 {
-    internal abstract class ViewModel : INotifyPropertyChanged
+    internal abstract class ViewModel : INotifyPropertyChanged, IDisposable
     {
+        //Шаблон IDisposable, для классов поддерживающих наследование
         public event PropertyChangedEventHandler PropertyChanged;
 
         protected virtual void OnPropertyChanged([CallerMemberName] string PropertyName = null)
@@ -29,5 +30,27 @@ namespace CV19.ViewModels.Base
             //Если св-во изменилось, то с помощью флагов ложь или истина, мы можем выполнить ещё какую-нибудь работу (по обновлению других св-в,например, которые с ним связаны)
 
         }
+        //Если вдруг будет деструктор, то вызываем Dispose(false) с флагом (false)
+        /*~ViewModel()
+        {
+            Dispose(false);
+        }
+*/
+        public void Dispose() 
+        {
+            Dispose(true);
+        }
+        private bool _Disposed;
+        //Dispose() специальный параметр Disposing, он будет protected virtual, поэтому наследники смогут его переопределить.
+        //Внутри метода Dispodse(), вызываем Dispose(true) с флагом (true)
+
+        protected virtual void Dispose(bool Disposing)
+        {
+            if (!Disposing || _Disposed) return;
+            _Disposed = true;
+            // Освобождение управляемых ресурсов
+        }
+
+        
     }
 }
