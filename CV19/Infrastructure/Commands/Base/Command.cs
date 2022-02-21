19b4421ac4e6232ce1632253238e8232ba951059
Илья Емец <67930111+Dzirt089@users.noreply.github.com>
2022-02-21 20:48:22 +0300

    Базовый класс команды

---
+++ CV19/Infrastructure/Commands/Base/Command.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+
+namespace CV19.Infrastructure.Commands.Base
+{
+    internal abstract class Command : ICommand
+    {
+        //Передаем управление этим событием системе WPF:
+        public event EventHandler CanExecuteChanged
+        {
+            add => CommandManager.RequerySuggested += value; 
+            remove => CommandManager.RequerySuggested -= value;
+        }
+        // Функция CanExecute возвращает либо истину либо ложь
+        //Если приходит ложь, то команду выполнить нельзя и элемент откл автоматически
+        //Если хочу выключить работу с каким-либо визуальным элементом, то у CanExecute возвращаем ложь
+        public abstract bool CanExecute(object parameter);
+        //Метод Execute - это то, что должно быть выполненно основной командой. Основная логика команды в Execute
+        public abstract void Execute(object parameter);
+}
